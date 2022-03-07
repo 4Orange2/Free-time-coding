f9a215e7150d48c7e466b2b927eb99d21a8c91fa
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2022-03-07 13:09:01 -0500

    Create Fraction_calc.py

---
+++ Fraction_calc.py
@@ -0,0 +1,18 @@
+# this is a fraction calculator
+denom = list(input("Denominators: ").split(","))
+numer = list(input("Numerators: ").split(","))
+def get_lcm(values):
+    ordered_denom = []
+    if denom[1] != denom[1]:
+        while exit < len(values):
+            greatest = denominator.pop(max(denominator))
+            print(greatest)
+        while(True):
+           if (greater % x == 0) and (greater % y == 0):
+               lcm = greater
+               break
+           greater += 1
+           exit += 1
+    #elif denom[1] == denom[1]:
+    
+print(numerator)
