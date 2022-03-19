094a480cf9e492a2f3cf02d4f0a0761a392b8c82
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2022-03-19 12:42:44 -0400

    adding postal code checker

--- for_iteration_practice.py
+++ for_iteration_practice.py
@@ -20,8 +20,8 @@ def test_has_digit():
 test_has_digit()
 
 # Exercise 2 - Use a for loop to write a function my_power that consumes two inputs base and exponent, 
-where base is a non-negative integer or floating point number and exponent is a non-negative integer. 
-Your function should produce base to the exponent power. The computation should be made using repeated multiplication.
+# where base is a non-negative integer or floating point number and exponent is a non-negative integer. 
+# Your function should produce base to the exponent power. The computation should be made using repeated multiplication.
 
 def my_power(base, exponent):
     ''' Calculates base to the exponent
@@ -40,7 +40,7 @@ print(my_power(2, 3))
 print(my_power(5, 4))
 
 # Exercise 3 - Use a for loop to write a function no_nums that consumes a string and produces a string which has the same characters 
-as the input but with all digits removed.
+# as the input but with all digits removed.
 
 def no_nums(string):
     listed = list(string)
@@ -56,8 +56,8 @@ print(no_nums("cat"))
 print(no_nums("2e"))
 
 # Exercise 4 - Use a for loop to write a function swap_case that consumes a string and produces a string which has the same characters 
-as the input but with any lower-case letter replaced by the corresponding upper-case letter and any upper-case letter replaced by 
-the corresponding lower-case letter.
+# as the input but with any lower-case letter replaced by the corresponding upper-case letter and any upper-case letter replaced by 
+# the corresponding lower-case letter.
 
 def swap_case(string):
     new_string = ""
@@ -73,4 +73,108 @@ def swap_case(string):
 print(swap_case("Hello"))
 print(swap_case("Hello1"))
 
-# Exercise 5 - 
+# Exercise 5 - A Canadian postal code is of the form "A9A 9A9", where 9 represents a digit and A represents an upper-case letter. 
+# Fill in the helper functions and the main function, using for loops in the helper functions.
+# Do not allow lower-case letters.
+
+def all_upper(entry):
+    """Determines if a string is all upper-case letters
+
+       Preconditions:
+       entry: string
+
+       Parameter:
+       entry: candidate string to check
+
+       Returns: True if so, False otherwise
+    """
+    for char in entry:
+        if char.isupper():
+            condition = True
+        else:
+            condition = False
+    return condition
+
+def all_digit(entry):
+    """Determines if a string is all digits
+
+       Preconditions:
+       entry: string
+
+       Parameter:
+       entry: candidate string to check
+
+       Returns: True if so, False otherwise
+    """
+    for char in entry:
+        if char.isdigit():
+            condition = True
+        else:
+            condition = False
+    return condition
+
+def get_even_numbers(entry):
+    even = ""
+    print("---")
+    print("These are the even numbers.")
+    for index in range(len(entry)):
+        if index % 2 == 0:
+            even += entry[index]
+            print("This is the main string: " + entry)
+            print("This is the even number: " + even)
+    return even
+    
+def get_odd_numbers(entry):
+    odd = ""
+    print("---")
+    print("These are the odd numbers.")
+    for index in range(len(entry)):
+        if index % 2 == 1:
+            odd += entry[index]
+            print("This is the main string: " + entry)
+            print("This is the odd number: " + odd)
+    return odd
+
+def merge_string(entry_1, entry_2):
+    merged = ""
+    print("---")
+    for index in range(len(entry_1)):
+        substr_merge = entry_1[index] + entry_2[index]
+        merged += substr_merge
+        print("This is the merged substring: " + substr_merge)
+        print("This is the merged string: " + merged)
+    return merged
+
+def postal_code(entry):
+    """Determines if a string is A9A 9A9 in form.
+
+       Preconditions:
+       entry: string
+
+       Parameter:
+       entry: candidate string to check
+
+       Returns: True if so, False otherwise
+    """
+    index = 0
+    condition = False
+    nums = ""
+    alpha = ""
+    if len(entry) == 7 and entry[3] == " ":
+        stripped_entry = entry.replace(" ", "")
+        print("This is the stripped entry: " + stripped_entry)
+        even = get_even_numbers(stripped_entry)
+        print("These are the letters: " + even)
+        odd = get_odd_numbers(stripped_entry)
+        print("These are the numbers: " + odd)
+        uppercase = all_upper(even)
+        digit = all_digit(odd)
+        if not digit or not uppercase:
+            return "Error - invalid syntax. Ensure that your postal code is in the A9A 9A9 form. Where 9 represents a digit and A represents an upper-case letter."
+        else:
+            postal_code = list(merge_string(even, odd))
+            postal_code.insert(3, " ")
+            str_pc = ''.join(postal_code)
+            print("---")
+            return str_pc
+print(postal_code("A9A 9A9"))
