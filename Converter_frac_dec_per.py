821d8ed72d8b296568c180e585e1ddceca8a5add
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2022-03-31 14:41:10 -0400

    Took out the slicing for certain conversions

--- Converter_frac_dec_per.py
+++ Converter_frac_dec_per.py
@@ -1,9 +1,10 @@
+'''
 # This is a fractions, decimals, and percentage convertor
 # Remember to put "frac", "dec", or "per"
 original = input("Input: ")
 og = original.split(" ")
 user = (input("To what? ").split(" "))
-
+'''
 def simplify(numer, denom):
     exit = 0
     if numer > denom:
@@ -19,8 +20,8 @@ def simplify(numer, denom):
             if gcf == False:
                 factor -= 1
             elif gcf == True:
-                numer = int(numer / factor)
-                denom = int(denom / factor)
+                numer = float(numer / factor)
+                denom = float(denom / factor)
                 exit += 1
             else:
                 print("error")
@@ -35,27 +36,36 @@ def convert(og, index, user):
             fraction = simplify(frac, 100)
         else:
             dec = handler_string[handler_string.index(".") + 1:]
-            fraction = simplify(int(dec), 10 ** len(dec))
+            fraction = simplify(float(dec), 10 ** len(dec))
         return fraction
 
     elif str(user[index]) == "dec":
         handler_string = og[index]
         if handler_string.count("/") > 0:
             handler_string = handler_string.split("/")
-            dec = int(handler_string[0]) / int(handler_string[1])
+            dec = float(handler_string[0]) / float(handler_string[1])
         else:
             handler_string = handler_string.replace("%", "")
-            dec = int(handler_string[:2]) / 100
+            dec = float(handler_string) / 100
         return dec
     elif str(user[index]) == "per":
         handler_string = og[index]
         if handler_string.count("/") > 0:
-            handler_string = handler_string.replace("/", "")
-            per = int(handler_string[0]) / int(handler_string[1]) * 100
+            handler_string = handler_string.split("/")
+            per = float(handler_string[0]) / float(handler_string[1]) * 100
         else:
-            per = int(handler_string[:2]) * 100
+            per = float(handler_string) * 100
         return (str(per) + "%")
-
+'''
 for index in range(len(og)):
     converted = convert(og, index, user)
     print(converted)
+'''    
+def test_convert():
+    assert convert(['3/4', '50/60', '1256.2'], 0, ['per'])
+    assert convert(['1/5', '50/60', '1256.2'], 2, ['frac', 'per', 'frac'])
+    assert convert(['3/4', '50/60', '1256.2'], 1, ['dec', 'dec'])
+    assert convert(['43%', '50/60', '1256.2'], 0, ['frac'])
+    assert convert(['35%', '50/60', '1256.2'], 0, ['dec'])
+    
+test_convert()
